1d78a6be2cbc5afb0b93878fc76372b198da7e2a
Ayyaz Zafar <ayyaz.zaffar@gmail.com>
2024-05-06 04:51:08 +0500

    Add message icon to header

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 import Title from './components/Title/Title';
+import globalStyle from './assets/styles/globalStyles';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Title title="Let's Explore" />
+      <View style={globalStyle.header}>
+        <Title title="Let's Explore" />
+        <TouchableOpacity style={globalStyle.messageIcon}>
+          <FontAwesomeIcon
+            size={20}
+            icon={faEnvelope}
+            style={{
+              color: '#898DAE',
+            }}
+          />
+
+          <View style={globalStyle.messageNumberContainer}>
+            <Text style={globalStyle.messageNumber}>3</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
