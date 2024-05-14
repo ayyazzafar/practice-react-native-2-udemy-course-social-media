2585dced5e0caa71af5952fedb8cc3653021ae46
Ayyaz Zafar <ayyaz.zaffar@gmail.com>
2024-05-14 23:33:02 +0500

    chore: Update scaling.ts for responsive design and device compatibility

--- assets/styles/scaling.ts
+++ assets/styles/scaling.ts
@@ -1,5 +1,15 @@
 import {Dimensions} from 'react-native';
-
+import DeviceInfo from 'react-native-device-info';
 const {width, height} = Dimensions.get('window');
 
-const isSmall = width < 375;
+const isSmall = width < 375 && !DeviceInfo.hasNotch();
+
+const guidelineBaseWidth = () => {
+  if (isSmall) {
+    return 330;
+  }
+
+  return 350;
+};
+
+const horizontalScale = (size: number) => (width / guidelineBaseWidth()) * size;
