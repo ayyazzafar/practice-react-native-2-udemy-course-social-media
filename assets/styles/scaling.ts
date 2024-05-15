f96636ceb8c1089b971dea0f1b1477feefcce21a
ayyaztech <ayyaz.zaffar@gmail.com>
2024-05-15 21:09:42 +0500

    chore: Update scaling.ts for responsive design and device compatibility

--- assets/styles/scaling.ts
+++ assets/styles/scaling.ts
@@ -7,9 +7,34 @@ const isSmall = width < 375 && !DeviceInfo.hasNotch();
 const guidelineBaseWidth = () => {
   if (isSmall) {
     return 330;
+  } else if (width > 410) {
+    return 400;
   }
 
   return 350;
 };
 
+const guidelineBaseHeight = () => {
+  if (isSmall) {
+    return 550;
+  } else if (width > 410) {
+    return 620;
+  }
+
+  return 680;
+};
+
 const horizontalScale = (size: number) => (width / guidelineBaseWidth()) * size;
+
+const verticalScale = (size: number) => (height / guidelineBaseHeight()) * size;
+
+const guidelineBaseFonts = () => {
+  if (width > 410) {
+    return 430;
+  } else return 400;
+};
+
+const scaleFontSize = (size: number) =>
+  Math.round((width / guidelineBaseFonts()) * size);
+
+export {horizontalScale, verticalScale, scaleFontSize};
